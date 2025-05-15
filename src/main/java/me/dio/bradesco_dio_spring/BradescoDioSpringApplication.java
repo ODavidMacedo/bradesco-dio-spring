0d15b79b39b8fa4ee82f112d4576a15f46a96362
David Macedo <david.macedo.da.silva@gmail.com>
2025-05-15 15:38:34 -0300

    Initial commit

---
+++ src/main/java/me/dio/bradesco_dio_spring/BradescoDioSpringApplication.java
@@ -0,0 +1,13 @@
+package me.dio.bradesco_dio_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BradescoDioSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BradescoDioSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/bradesco_dio_spring/BradescoDioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.bradesco_dio_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BradescoDioSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
